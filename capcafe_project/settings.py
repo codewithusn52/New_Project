df31463d939b39f94abe8a0c00411c70902b8c16
Sk_Husna <shaikhhusnaazeezullah@gmail.com>
2026-09-25 19:00:36 +0530

    Update settings.py

--- capcafe_project/settings.py
+++ capcafe_project/settings.py
@@ -1,1 +1,20 @@
-settings.py
+INSTALLED_APPS = [
+    # ... standard django apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Local Apps
+    'cafe',
+]
+
+# Static and Media Files configuration
+import os
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
